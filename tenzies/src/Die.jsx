48bbecf8b3fec6d790a23feb0f9d7fc976ddf64a
ray-17 <simonsharon144@gmail.com>
2026-03-10 02:57:56 +0100

    changed numbers to pips

--- tenzies/src/Die.jsx
+++ tenzies/src/Die.jsx
@@ -3,13 +3,34 @@
 
 export default function Die(props) {
 
+    const pipLayouts = {
+        1: [5],
+        2: [1, 9],
+        3: [1, 5, 9],
+        4: [1, 3, 7, 9],
+        5: [1, 3, 5, 7, 9],
+        6: [1, 3, 4, 6, 7, 9]
+    }
+
+    const positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
 
     return (
-        <button onClick={() => props.hold(props.id)}
+        <button className="die-container" onClick={() => props.hold(props.id)}
             style={{ backgroundColor: props.isHeld ? "#59E391" : "white" }}
             aria-pressed={props.isHeld}
             aria-label={`Die with value ${props.value}, 
             ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
+        > {
+                positions.map(position => {
+                    const hasPip = pipLayouts[props.value].includes(position)
+
+                    return (
+                        <div key={position} className="cell">
+                            {hasPip && <div className="pip" key={position}></div>}
+                        </div>
+                        )
+                })}</button>
     )
 }
